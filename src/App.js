aeed1de02d2eeb01c7b2504b98c4ba270033dee5
Tingying Ding <dty@fb.com>
2024-06-09 11:57:54 -0700

    remove manifest.json

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import './index.css';
 export default function App() {
   return (
     <div>
-    <div>
-    <button type="button" class="add-to-cart-btn">AddToCart</button>
-    </div>
-    <div>
-    <button type="button" class="purchase-btn">Purchase</button>
-    </div>
+      <div>
+        <button type="button" class="add-to-cart-btn">AddToCart</button>
+      </div>
+      <div>
+        <button type="button" class="purchase-btn">Purchase</button>
+      </div>
     </div>
   )
 }
